94617da80e356319d521700d8bef732d40d9e247
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2022-05-22 23:34:07 +0700

    feat: slicing sign in and sign up

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,29 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.js',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js",
     ],
 
     theme: {
+        fontFamily: {
+            poppins: "Poppins, sans-serif",
+        },
         extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            colors: {
+                alerange: "#FB6908",
+                "gray-1": "#B4B4B4",
+                "gray-2": "#E2E0E0",
+                "form-bg": "#212121",
+            },
+            screens: {
+                laptopLg: "1160px",
+                laptopXl: "1360px",
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
