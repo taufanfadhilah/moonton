7f6acbc3560298b8b2dea3a3fbe518a7c263b647
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2022-05-27 23:07:16 +0700

    feat: integration sign in

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Inertia\Inertia;
 |
 */
 
-Route::redirect('/', '/prototype/login');
+Route::redirect('/', '/login');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
